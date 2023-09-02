f6c7f59ea83f012125a0320335e6d879c91c32c3
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-09-02 23:31:32 +0530

    first commit

---
+++ basic program/01.js
@@ -0,0 +1,11 @@
+// user se mango ek number and agar number 10 se jyada ho to hello print kardo nhi tho hey print kardo
+
+let number= prompt("number batao");
+
+if(number>10){
+    console.log("Hello");
+}
+
+else {
+    console.log("Hey");
+}
