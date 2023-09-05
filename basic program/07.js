73c130d0d61a42d23c5387ed801aa8b9f4491d73
NURUZZAMAN <117540775+nzaman7878@users.noreply.github.com>
2023-09-05 18:49:32 +0530

    Add files via upload

---
+++ basic program/07.js
@@ -0,0 +1,22 @@
+// else if
+
+let num = prompt("number : " );
+
+num = Number(num);
+
+if (num > 100) {
+
+    console.log("badhiya");
+}
+
+else if (num>80) {
+    console.log("kaafi accha");
+
+}
+
+else if (num>60) {
+    console.log("accha");
+}
+else{
+    console.log("bekkar");
+}
