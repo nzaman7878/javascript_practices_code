c55c3ccb6801765ac4a0ca69b28a1d2136174c43
NURUZZAMAN <117540775+nzaman7878@users.noreply.github.com>
2023-09-05 18:48:54 +0530

    Add files via upload

---
+++ basic program/04.js
@@ -0,0 +1,12 @@
+// user se passwords mango and agar match kar jaaaye login karo nahito mana karo
+
+let pass1 = prompt("pass dedo : ");
+let pass2 = prompt("pass dobara dedo " );
+
+if (pass1 === pass2){
+    console.log("Login is allowed: ");
+}
+
+else{
+    console.log("Login is not allowed : ");
+}
