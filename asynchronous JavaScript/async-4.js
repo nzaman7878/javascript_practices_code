ded279aa8bba25d788d7e2cb7c565f7555fb2468
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-25 23:36:47 +0530

    async 4

---
+++ asynchronous JavaScript/async-4.js
@@ -0,0 +1,11 @@
+//Reading a File with Callbacks:
+
+const fs = require('fs');
+
+fs.readFile('example.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
