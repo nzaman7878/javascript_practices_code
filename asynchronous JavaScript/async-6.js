5bbbd286c0133cd4ffac522d35645092a87f4ff4
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-25 23:57:18 +0530

    promises

---
+++ asynchronous JavaScript/async-6.js
@@ -0,0 +1,14 @@
+// Promises
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Promise resolved after 1 second');
+    }, 1000);
+  });
+  
+  myPromise.then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.error('Error:', error);
+  });
+  
