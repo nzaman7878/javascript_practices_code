df836aa30a8293e85ccbc95d93c6e8a19450a210
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-29 23:39:14 +0530

    Error Handling with Try-Catch

---
+++ Error Handling/err4.js
@@ -0,0 +1,11 @@
+// Error Handling with Try-Catch:
+
+try {
+    // Code that may throw an error
+    const result = 10 / 0; // Division by zero
+    console.log(result);
+  } catch (error) {
+    // Catching and handling the error
+    console.error('An error occurred:', error);
+  }
+  
