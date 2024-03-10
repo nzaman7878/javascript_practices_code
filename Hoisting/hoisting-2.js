a88a2a54ce689bbe89fdadaacc35e46b152076d7
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-03-10 23:26:21 +0530

    hoisting eg 2

---
+++ Hoisting/hoisting-2.js
@@ -0,0 +1,6 @@
+var x;
+console.log(x);
+x = 5;
+
+// As a result, var x; is hoisted to the top,
+//  and when console.log(x) is executed, x is declared but not yet assigned a value, hence it logs undefined.
