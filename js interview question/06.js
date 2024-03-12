8122843b6c4a3858c6d1d270375e3bfbb2290486
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-03-13 01:28:15 +0530

    js q6 explanation

--- js interview question/06.js
+++ js interview question/06.js
@@ -47,4 +47,18 @@ function fibonacciWithoutMemoization(n) {
   console.time('Fibonacci with memoization');
   console.log('Fibonacci number (with memoization):', fibonacciMemoized(40));
   console.timeEnd('Fibonacci with memoization');
-  
+
+  /*
+  In this example:
+
+We define a fibonacciWithoutMemoization function that calculates the nth Fibonacci number recursively without memoization.
+We define a fibonacciWithMemoization function that returns a closure with memoization. Inside the closure,
+ we maintain a cache object to store previously calculated Fibonacci numbers.
+When calculating the Fibonacci number with memoization, we first check if the result is already cached. 
+If it is, we return the cached result; otherwise, we calculate it recursively and store it in the cache for future use.
+We compare the performance of both approaches by measuring the execution time using console.time() and console.timeEnd().
+You'll notice that the Fibonacci function with memoization executes significantly faster for 
+large values of n compared to the function without memoization.
+ Memoization helps avoid redundant calculations by caching the results, resulting in improved performance.
+  */
+ 
